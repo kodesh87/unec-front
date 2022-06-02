3f733c43053509bb8b635eea707b6e15e21f0212
adham618 <adhamtarek291@gmail.com>
2022-06-02 20:22:04 +0200

    complete mobile nav

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,13 @@ $(".toggle-btn").click(function () {
 $(".toggle-menu-btn").click(function () {
   $("#mobile-menu").toggle();
 });
+
+// accordion;
+
+$(".panel-collapse").on("show.bs.collapse", function () {
+  $(this).siblings(".panel-heading").addClass("active");
+});
+
+$(".panel-collapse").on("hide.bs.collapse", function () {
+  $(this).siblings(".panel-heading").removeClass("active");
+});
