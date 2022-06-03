58052d3f40cfc145dd05fe0c1a1471b0727539ed
adham618 <adhamtarek291@gmail.com>
2022-06-03 16:34:50 +0200

    finish rəqəmsal section

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,21 @@ $(document).on("click", function () {
 $(document).on("click", ".menu-content", function (e) {
   e.stopPropagation();
 });
+//myCarousel
+$("section.rəqəmsal .carousel .carousel-item").each(function () {
+  var minPerSlide = 4;
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+
+    next.children(":first-child").clone().appendTo($(this));
+  }
+});
